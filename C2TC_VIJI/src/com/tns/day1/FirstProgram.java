d9ca3cfb05a82567436b9064cdb8226d771b32cc
Hp <Hp@viji>
2024-09-04 11:51:54 +0530

    This is my first commit

---
+++ C2TC_VIJI/src/com/tns/day1/FirstProgram.java
@@ -0,0 +1,40 @@
+package com.tns.day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100;
+		int b=90;
+		//arithmetic
+		System.out.println("a+b="+(a+b));
+		System.out.println("a-b="+(a-b));
+		System.out.println("a/b="+(a/b));
+		System.out.println("a*b="+(a*b));
+		System.out.println("a%b="+(a%b));
+		//increment 
+		System.out.println("a++="+a);
+		//decrement
+		System.out.println("a--="+a);
+		
+		//relational 
+		System.out.println("a<b="+(a<b));
+		System.out.println("a>=b="+(a>=b));
+		System.out.println("a<=b="+(a<=b));
+		System.out.println("a<b="+(a<b));
+	    System.out.println("a==b="+(a==b));
+	    //assignment
+		System.out.println("a=b="+(a=b));
+		
+		System.out.println("a+=b="+(a+=b));
+		System.out.println("a-=b="+(a-=b));
+		System.out.println("a*=b="+(a*=b));
+		System.out.println("a/=b="+(a/=b));
+		System.out.println("a%=b="+(a%=b));
+		//ternary
+		System.out.println((a>b)?"hi":"hello");
+
+
+	}
+
+}
